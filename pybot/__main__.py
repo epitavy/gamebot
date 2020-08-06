457ff33a8bb24de47f1cd8a0b98b2e427cfef63b
Eliaz Pitavy <eliaz.pitavy@epita.fr>
2020-08-06 13:06:21 +0200

    Capitalize game name in usage print
    Signed-off-by: Eliaz Pitavy <eliaz.pitavy@epita.fr>

--- pybot/__main__.py
+++ pybot/__main__.py
@@ -2,14 +2,17 @@ if __name__ == "__main__":
     import sys
     import pybot
 
+    GAMES = {"tictactoe": "import pybot.games.tictactoe as game"}
+
     if len(sys.argv) != 2:
-        print(f"Usage:\n\t{sys.argv[0]} <game>\nGames:\n\ttictactoe", file=sys.stderr)
+        print(f"Usage:\n\t{sys.argv[0]} <game>\nGames:\n", end="", file=sys.stderr)
+        for game in GAMES:
+            print(f"\t{game.capitalize()}")
         exit(1)
     else:
         if sys.argv[1] not in games:
             print(f"The game {sys.argv[1]} is not available", file=sys.stderr)
             exit(1)
 
-        games = {"tictactoe": "import pybot.games.tictactoe as game"}
         exec(games[sys.argv[1]])
         game.run()
