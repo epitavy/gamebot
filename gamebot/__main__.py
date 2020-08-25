ce98d0d48a99f7f76442fb2e8580ab1da0923268
Eliaz Pitavy <eliaz.pitavy@epita.fr>
2020-08-25 15:32:26 +0200

    Remove useless condition
    Signed-off-by: Eliaz Pitavy <eliaz.pitavy@epita.fr>

--- gamebot/__main__.py
+++ gamebot/__main__.py
@@ -1,18 +1,17 @@
-if __name__ == "__main__":
-    import sys
-    import gamebot
+import sys
+import gamebot
 
-    GAMES = {"tictactoe": "import gamebot.games.tictactoe as game"}
+GAMES = {"tictactoe": "import gamebot.games.tictactoe as game"}
 
-    if len(sys.argv) != 2:
-        print(f"Usage:\n\t{sys.argv[0]} <game>\nGames:\n", end="", file=sys.stderr)
-        for game in GAMES:
-            print(f"\t{game.capitalize()}")
+if len(sys.argv) != 2:
+    print(f"Usage:\n\t{sys.argv[0]} <game>\nGames:\n", end="", file=sys.stderr)
+    for game in GAMES:
+        print(f"\t{game.capitalize()}")
+    exit(1)
+else:
+    if sys.argv[1] not in GAMES:
+        print(f"The game {sys.argv[1]} is not available", file=sys.stderr)
         exit(1)
-    else:
-        if sys.argv[1] not in GAMES:
-            print(f"The game {sys.argv[1]} is not available", file=sys.stderr)
-            exit(1)
 
-        exec(GAMES[sys.argv[1]])
-        game.run()
+    exec(GAMES[sys.argv[1]])
+    game.run()
